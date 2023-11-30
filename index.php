4397631068cb28f531c5f495395ee3ac3e7ddda1
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-30 15:20:47 +0100

    scaffolding

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php include_once __DIR__ . "/partials/header.php";
+
+
+?>
+
+<main class="container w-100">
+    <div class="">
+        <form action="" method="GET">
+            <input type="number" min='6' max='20' name="passwordLen">
+            <button type="submit">Genera</button>
+            <button type="reset">Reset</button>
+        </form>
+    </div>
+
+</main>
+</body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Strong Password Generator</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="bg-warning">
+
+    <header class="container mt-5 text-center">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+    </header>
